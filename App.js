6abc94abe9cb859a293f5b45649a747ef33134e7
Vanessa Wendling <82171234+VanessaWendling@users.noreply.github.com>
2021-06-21 15:08:45 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import List from "./pages/List";
+import Repository from "./pages/Repository";
+import GlobalStyle from "./styles";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={List} />
+        <Route path="/repository/:repository+" component={Repository} />
+      </Switch>
+      <GlobalStyle />
+    </BrowserRouter>
+  );
+}
+
+export default App;
